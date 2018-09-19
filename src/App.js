acacf6e0d43ee1887eec449f5f78416d5ba7f7f6
JobbyM <songzhipengmune@gmail.com>
2018-09-19 17:20:38 +0800

    react-transition-group的使用(2)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import React, { Component, Fragment } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import './style.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      show: true
+      list: []
     }
 
-    this.handleBtnToggle = this.handleBtnToggle.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in={this.state.show}
-          timeout={1000}
-          classNames="fade"
-          unmountOnExit
-          onEntered={(el)=> {el.style.color = 'red'}}
-          appear={true}
-        >
-          <div>hello</div>
-        </CSSTransition>
-        <button onClick={this.handleBtnToggle}>toggle</button>
+        <TransitionGroup>
+        {
+          this.state.list.map((item, index) => {
+            return (
+              <CSSTransition
+                key={index}
+                timeout={1000}
+                classNames="fade"
+                unmountOnExit
+                onEntered={(el)=> {el.style.color = 'red'}}
+                appear={true}
+              >
+                <div>{item}</div>
+              </CSSTransition>
+            )
+          })
+        }
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>toggle</button>
       </Fragment>
     )
   }
 
-  handleBtnToggle() {
+  handleAddItem() {
     this.setState((preState) => ({
-      show: !preState.show
+      list: [...preState.list, 'item']
     }))
   }
 }
